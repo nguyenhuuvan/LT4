d920b49d259f0542420a0191923a79f9e53d95b6
Nguyen Huu Van <vannhph06247@fpt.edu.vn>
2019-08-28 19:33:02 +0700

    đâsd

--- app/src/main/java/com/poly/lt4/Main2Activity.java
+++ app/src/main/java/com/poly/lt4/Main2Activity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
-
+    boolean a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
     }
 }
