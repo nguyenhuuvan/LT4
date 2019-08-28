a950e3a84370497d0f19043758f60736f2b78012
Nguyen Huu Van <vannhph06247@fpt.edu.vn>
2019-08-28 19:42:29 +0700

    abcd

--- app/src/main/java/com/poly/lt4/Main2Activity.java
+++ app/src/main/java/com/poly/lt4/Main2Activity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
-    boolean b;
+    boolean c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
